9f84da1dc2239bdd8c828e85f974b82c6cba7884
Terry Tao <librakevin@gmail.com>
2013-05-19 10:53:08 +0800

    Adding bash/calc-memory.sh

---
+++ bash/calc-memory.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+for PROC in `ls /proc/|grep "^[0-9]"`
+do
+  if [ -f /proc/$PROC/statm ]; then
+    TEP=`cat /proc/$PROC/statm | awk '{print ($2)}'`
+    RSS=`expr $RSS + $TEP`
+  fi
+done
+RSS=`expr $RSS \* 4`
+PageTable=`grep PageTables /proc/meminfo | awk '{print $2}'`
+SlabInfo=`cat /proc/slabinfo |awk 'BEGIN{sum=0;}{sum=sum+$3*$4;}END{print sum/1024/1024}'`
+
+echo -e "RSS\tPageTable\tSlabInfo"
+echo -e $RSS" KB\t"$PageTable" KB\t"$SlabInfo" MB"
+printf "rss+pagetable+slabinfo=%sMB\n" `echo $RSS/1024 + $PageTable/1024 + $SlabInfo|bc`
+free -m
