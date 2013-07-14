453fd512e20acb773b31e35698a76dccbd1ad955
Terry Tao <librakevin@gmail.com>
2013-07-14 17:01:36 +0800

    Experimenting more features in Squeryl.

--- scala/squeryl-example/sql/create_data.sql
+++ scala/squeryl-example/sql/create_data.sql
@@ -1,10 +1,27 @@
+drop table if exists POSITION;
 drop table if exists ACCOUNT;
+
 create table ACCOUNT (
-       id bigint(20),
-       owner varchar(100),
-       capital decimal
+       ID bigint(20) not null,
+       OWNER varchar(100) not null,
+       CAPITAL decimal not null,
+       primary key (ID)
 );
+
+create table POSITION (
+       ID bigint(20) not null,
+       ACCOUNT_ID bigint(20) not null,
+       CAPITAL decimal not null,
+       primary key (ID),
+       foreign key (ACCOUNT_ID) references ACCOUNT (ID)
+);
+
 insert into ACCOUNT values (1, 'Terry Tao', 100000);
 insert into ACCOUNT values (2, 'Hongsi Hu', 20000);
 insert into ACCOUNT values (3, 'Chaoran Zheng', 2000);
 insert into ACCOUNT values (4, 'Yanyan Shen', 2000);
+
+insert into POSITION values (1, 1, 50000);
+insert into POSITION values (2, 1, 100000);
+insert into POSITION values (3, 2, 3000);
+insert into POSITION values (4, 3, 50000);
--- scala/squeryl-example/src/main/scala/example/TestSqueryl.scala
+++ scala/squeryl-example/src/main/scala/example/TestSqueryl.scala
@@ -4,6 +4,7 @@ import scala.math.BigDecimal
 import scala.math.BigDecimal._
 import java.math.{ BigDecimal => JBigDecimal }
 
+import org.squeryl.dsl
 import org.squeryl.PrimitiveTypeMode._
 import org.squeryl.{ Schema, Session, SessionFactory }
 import org.squeryl.adapters.MySQLAdapter
@@ -16,21 +17,80 @@ object TestSqueryl {
 
   private def testSqueryl(): Unit = {
     Class.forName("com.mysql.jdbc.Driver");
-    SessionFactory.concreteFactory = Some(() =>
-      Session.create(
+    SessionFactory.concreteFactory = Some { () =>
+      val session = Session.create(
         java.sql.DriverManager.getConnection(
           "jdbc:mysql://localhost:3306/myaccount", "root", ""),
-        new MySQLAdapter))
+        new MySQLAdapter)
+      session.setLogger( statement => println("GENERATED SQL: " + statement) )
+      session
+    }
 
+    testSelectOuterJoinSum
+    //testSelectOuterJoin
+    //testSelectJoin
+    //testSelect
     //testInsert
     //testUpdate
-    testDelete
+    //testDelete
+  }
 
+  private def testSelectOuterJoinSum(): Unit = {
+    import SecurityMaster._
+    transaction {
+      val accountCapitals = join(ACCOUNTS, POSITIONS.leftOuter)((acc, pos) =>
+        groupBy(acc.id, acc.owner)
+          compute( nvl(sum(pos.map(_.capital)), 0) )
+          on (acc.id === pos.map(_.accountId))
+      )
+      for (accCap <- accountCapitals) {
+        println(accCap.key + "  " + accCap.measures)
+      }
+    }
+  }
+
+  private def testSelectOuterJoin(): Unit = {
+    import SecurityMaster._
+    transaction {
+      val accountsPositions = join(ACCOUNTS, POSITIONS.leftOuter)((acc, pos) =>
+        select(acc, pos)
+          on (acc.id === pos.map(_.accountId))
+      )
+      for (accPos <- accountsPositions) {
+        val (acc, posOpt) = accPos
+        val builder = new StringBuilder
+        builder.append("AccountOwner=" + acc.owner)
+        builder.append("  AccountId=" + acc.id)
+        posOpt match {
+          case Some(pos) =>
+            builder.append("  PositionId=" + pos.id)
+            builder.append("  PositionCapital=" + pos.capital)
+          case None =>
+        }
+        println(builder.toString)
+      }
+    }
+  }
+
+  private def testSelectJoin(): Unit = {
+    import SecurityMaster._
+    transaction {
+      val poss = from(ACCOUNTS, POSITIONS) ( (acc, pos) =>
+        where(acc.id === pos.accountId)
+          select(acc, pos)
+          orderBy(acc.id asc, pos.id desc)
+      )
+      for (p <- poss) {
+        val (acc, pos) = p
+        println(acc.owner + " " + acc.id + " " + pos.id + " " + pos.capital)
+        //println(p)
+      }
+    }
   }
 
   private def testDelete(): Unit = {
     transaction {
-      SecurityMaster.accounts.deleteWhere(a => a.id >= 10)
+      SecurityMaster.ACCOUNTS.deleteWhere(a => a.id.~ >= 10)
     }
   }
 
@@ -38,9 +98,8 @@ object TestSqueryl {
     val (_, d) = time {
       transaction {
         for (i <- 10 until 100) {
-          val a = new Account(Some(i), Some("hello" + i),
-            Some(BigDecimal(20000)))
-          SecurityMaster.accounts.insert(a)
+          val a = new Account(i, "hello" + i, BigDecimal(20000))
+          SecurityMaster.ACCOUNTS.insert(a)
         }
       }
     }
@@ -49,10 +108,9 @@ object TestSqueryl {
 
   private def testUpdate(): Unit = {
     transaction {
-      update(SecurityMaster.accounts) ( s =>
+      update(SecurityMaster.ACCOUNTS)(s =>
         where(s.id === 1)
-          set(s.capital := s.capital + 1000)
-      )
+          set (s.capital := s.capital + 1000))
       throw new Exception("hi")
       Unit
     }
@@ -62,7 +120,7 @@ object TestSqueryl {
     println("Test select")
     val (_, duration) = time {
       transaction {
-        val accounts = from(SecurityMaster.accounts) (s =>
+        val accounts = from(SecurityMaster.ACCOUNTS) ( s =>
           select(s)
         )
         for (a <- accounts) {
@@ -79,18 +137,28 @@ object TestSqueryl {
     val d = System.currentTimeMillis - t
     (r, d)
   }
+
 }
 
 class Account(
-  @Column("id") val id: Option[Long],
-  @Column("owner") val owner: Option[String],
-  @Column("capital") val capital: Option[BigDecimal]
+  @Column("ID") val id: Long,
+  @Column("OWNER") val owner: String,
+  @Column("CAPITAL") val capital: BigDecimal
 ) {
+  def this() = this(0L, "", BigDecimal("0"))
+}
 
-  def this() = this( Some(0L), Some(""), Some(BigDecimal("0")) )
+class Position(
+  @Column("ID") val id: Long,
+  @Column("ACCOUNT_ID") val accountId: Long,
+  @Column("CAPITAL") val capital: BigDecimal
+) {
+  def this() = this(0L, 0L, BigDecimal("0"))
 }
 
 object SecurityMaster extends Schema {
 
-  val accounts = table[Account]("ACCOUNT")
+  val ACCOUNTS = table[Account]("ACCOUNT")
+
+  val POSITIONS = table[Position]("POSITION")
 }
