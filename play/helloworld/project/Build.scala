be01b78214a1e835f844ec16fffb39a883fe8139
Terry Tao <librakevin@gmail.com>
2013-08-03 15:59:53 +0800

    Add play example: helloworld

---
+++ play/helloworld/project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+import play.Project._
+
+import com.github.play2war.plugin._
+
+object ApplicationBuild extends Build {
+
+    val appName         = "helloworld"
+    val appVersion      = "0.1"
+
+    val appDependencies = Seq(
+      // Add your project dependencies here,
+    )
+
+    val main = play.Project(appName, appVersion, appDependencies).settings(
+      // Add your own project settings here
+      Play2WarPlugin.play2WarSettings: _*
+    ).settings(
+      Play2WarKeys.servletVersion := "3.0"
+    )
+
+}
